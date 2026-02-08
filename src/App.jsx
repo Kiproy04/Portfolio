3b7a7ab630592054ddc1319dfe1e79369e8f7165
Leroy Kirui <124944229+Kiproy04@users.noreply.github.com>
2026-02-08 07:34:50 +0000

    Project setup and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import ProjectCard from "./components/ProjectCard";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>My Portfolio</h1>
+
+//       <ProjectCard
+//         title="Job Matching Platform"
+//         description="Connects employers with candidates"
+//       />
+//     </div>
+//   );
+// }
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Tailwind is finally working 🎉
+      </h1>
+    </div>
+  );
+}
