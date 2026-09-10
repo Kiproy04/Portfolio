c5c0ba00e0fe026efeb4b91dc89dae13e7dace3c
Kiproy04 <delleroy04@gmail.com>
2026-09-10 16:37:17 +0300

    Add dev-toolkit demo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,7 @@ import {
   SiTailwindcss,
   SiShell,
 } from "react-icons/si";
+import { siShell } from "simple-icons";
 
 export default function Projects() {
   const [hoveredProject, setHoveredProject] = useState(null);
@@ -78,7 +79,7 @@ export default function Projects() {
       image:
         "/dev-toolkit.png",
       tech: [
-        { name: "Shell", icon: SiShell, color: "text-green-400" },
+        { name: "Shell", icon: siShell, color: "text-green-400" },
       ],
       github: "https://github.com/Kiproy04/dev-toolkit",
       demo: null,
