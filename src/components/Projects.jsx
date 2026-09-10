e3a7febcd820d954359ab2d3ea08437dfd616cce
Kiproy04 <delleroy04@gmail.com>
2026-09-11 00:07:00 +0300

    Fix projects redundunt icon imports

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,9 +10,8 @@ import {
   SiDocker,
   SiRedis,
   SiTailwindcss,
-  SiShell,
+  SiGnubash
 } from "react-icons/si";
-import { siShell, siShelly } from "simple-icons";
 
 export default function Projects() {
   const [hoveredProject, setHoveredProject] = useState(null);
@@ -79,7 +78,7 @@ export default function Projects() {
       image:
         "/dev-toolkit.png",
       tech: [
-        { name: "Shell", icon: siShell, color: "text-green-400" },
+        { name: "Shell", icon: SiGnubash, color: "text-green-400" },
       ],
       github: "https://github.com/Kiproy04/dev-toolkit",
       demo: null,
